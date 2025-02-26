3823de3531fdd75aecca1bd9dfdba1cee5f03ebf
Vivek <vivekkalp@gmail.com>
2025-02-27 00:11:14 +0530

    dfd

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const router = require("./router/auth-router");
 const connectDB = require("./utils/db")
 
 var corsOptions = {
-    origin:  ["https://mindguardai.vercel.app","http://localhost:5173"],
+    origin:  ["https://mindguardai.vercel.app","http://localhost:5173/login"],
     method: "GET, POST, PUT, PATCH, HEAD",
     credential: true
 }
