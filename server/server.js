d25948afd2a3107631f4591bdc7c8a94f7493fa4
Vivek <vivekkalp@gmail.com>
2025-02-27 00:32:59 +0530

    cors

--- server/server.js
+++ server/server.js
@@ -5,13 +5,32 @@ const cors = require("cors");
 const router = require("./router/auth-router");
 const connectDB = require("./utils/db")
 
-var corsOptions = {
-    origin:  ["https://mindguardai.vercel.app","http://localhost:5173/login"],
-    method: "GET, POST, PUT, PATCH, HEAD",
-    credential: true
-}
+// var corsOptions = {
+//     origin:  ["https://mindguardai.vercel.app","http://localhost:5173/login"],
+//     method: "GET, POST, PUT, PATCH, HEAD",
+//     credential: true
+// }
 
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
+
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://mindguardai.vercel.app"
+];
+
+app.use(cors({
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("CORS not allowed"));
+    }
+  },
+  methods: "GET, POST, PUT, DELETE",
+  credentials: true
+}));
+
+
 app.use(express.json());
 
 app.use("/api/auth", router);
